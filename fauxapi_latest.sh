09c0d8c1a06e9fb11798e92f1568a4bae4e4ad07
dlvargas123 <66894336+dlvargas123@users.noreply.github.com>
2023-07-11 14:28:27 -0500

    Update fauxapi_latest.sh

--- fauxapi_latest.sh
+++ fauxapi_latest.sh
@@ -1,4 +1,4 @@
-set fauxapi_base_package_url='https://raw.githubusercontent.com/ndejong/pfsense_fauxapi_packages/master'
-set fauxapi_latest=`fetch -qo - ${fauxapi_base_package_url}/LATEST`
+fauxapi_base_package_url='https://raw.githubusercontent.com/ndejong/pfsense_fauxapi_packages/master'
+fauxapi_latest=`fetch -qo - ${fauxapi_base_package_url}/LATEST`
 fetch ${fauxapi_base_package_url}/${fauxapi_latest}
 pkg-static install ${fauxapi_latest}
