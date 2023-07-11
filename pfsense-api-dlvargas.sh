c4ef2a5c91116443edff4a96f9902d4ac38161c4
dlvargas123 <66894336+dlvargas123@users.noreply.github.com>
2023-07-11 16:17:10 -0500

    Update pfsense-api-dlvargas.sh

--- pfsense-api-dlvargas.sh
+++ pfsense-api-dlvargas.sh
@@ -15,7 +15,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Punto de control 3: Verificar la ejecución de fauxapi_dlvargas.sh
-echo "Y" | /usr/local/www/fauxapi_dlvargas.sh
+sh /usr/local/www/fauxapi_dlvargas.sh
 if [ $? -ne 0 ]; then
     echo "Error: Fallo al ejecutar fauxapi_dlvargas.sh"
     exit 1
@@ -30,3 +30,6 @@ fi
 # Punto de control final: Todo se ejecutó correctamente
 echo "El script se ejecutó correctamente"
 exit 0
+
+
+sh /usr/local/www/fauxapi_dlvargas.sh
